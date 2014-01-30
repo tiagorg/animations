3b0ce48a4b8bd0172a589639ba403313b6783d13
Guilherme <guilherme.goncalves@macys.com>
2014-01-30 14:49:54 -0200

    making it worst =)

--- bigframes.js
+++ bigframes.js
@@ -3,7 +3,7 @@ var countBigFrames = function () {
 	numberOfBigFrames = 0;
 	lisArray.forEach(function(li){
 		li.style.float = 'left';
-		li.style.border = '1px';
+		li.style.border = Math.floor((Math.random()*2)+1)+'px';
 		li.style.border += ' solid';
 		li.style.border += ' black';
 		li.style.margin = '4px';
--- events.js
+++ events.js
@@ -1,9 +1,11 @@
-var lisArray = countAnimations();
+var lisArray = getAnimations();
 lisArray.forEach(function(li) {
 	li.addEventListener('click', function(e) {
 		if (e.currentTarget.className !== 'happy') {
+			alert("Removing... Trust me, it will be removed.");
 			e.currentTarget.parentNode.removeChild(e.currentTarget);
 			countBigFrames();
+			alert("Removed!");
 		} else {
 			alert("Don't remove happiness from your site.");
 		}
